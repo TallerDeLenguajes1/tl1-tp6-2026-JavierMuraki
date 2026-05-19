1fe6842a734c8c59ce9b09f6423adba04e7f33c1
JavierMuraki <javimuraki@gmail.com>
2026-05-19 14:50:50 -0300

    Inicio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int Numero = 0;
+
+Console.Write("Igrese numero: ");
+string? Input = Console.ReadLine();
+
+if (int.TryParse(Input, out Numero)) {
+    Console.WriteLine(Numero);
+} else {
+    Console.WriteLine("Ingreso invalido: [" + Input + "] Se esperaba un numero.");
+}
