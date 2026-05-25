4f48c929a71d6988500db690cf2475541274038c
Javier Muraki <javimuraki@gmail.com>
2026-05-24 23:31:02 -0300

    Ejercicio 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ Console.Write("Igrese numero: ");
 string? Input = Console.ReadLine();
 
 if (int.TryParse(Input, out Numero)) {
-    Console.WriteLine(Numero);
+    if (Numero > 0) {
+        Console.WriteLine("Numero invertido: " + -Numero); 
+    } else {
+       Console.WriteLine("El numero tiene que ser mayor a 0.");
+    }
 } else {
     Console.WriteLine("Ingreso invalido: [" + Input + "] Se esperaba un numero.");
 }
